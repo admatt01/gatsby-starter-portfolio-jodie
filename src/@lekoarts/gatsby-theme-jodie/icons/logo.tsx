6e742a6e9da1ce4b925fe4422830efc453b7f754
admatt01 <admatt01@gmail.com>
2023-10-19 22:45:46 +1100

    commit

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import myLogo from "../logo.svg"; // Adjust the path to your SVG file
+import myLogo from "./logo.svg"; // Adjust the path to your SVG file
 
 const Logo = () => (
   <img src={myLogo} alt="My Logo" style={{ width: "150px", height: "auto" }} />
